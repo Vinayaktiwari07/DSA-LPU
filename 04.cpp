aeeb08a3cd11c5ea18b931d7134ed29bc08e4aec
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 14:05:54 +0530

    Create 04.cpp

---
+++ 04.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n= 315;
+    if(n % 2 == 0){
+        cout << "even" << endl;
+    }else{
+        cout << "Odd" << endl;
+    }
+    return 0;
+}
