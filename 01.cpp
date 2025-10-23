67762c955f7fccefac7d8b24030ef3a7e4766735
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 14:05:37 +0530

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout << "Enter input here: " ;
+    cin >> a >> b >> c;
+    cout << "addition is:" << a+b+c << endl;
+    cout << "multiplicetion is: " << a*b*c << endl;
+    cout << "subtraction is: " << a-b-c << endl;
+    return 0;
+} 
