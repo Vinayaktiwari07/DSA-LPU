eff9d7b46c593978fb7de2189abe6cfefee69944
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-23 14:06:01 +0530

    Create 06.cpp

---
+++ 06.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the input: " << endl;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n;i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
