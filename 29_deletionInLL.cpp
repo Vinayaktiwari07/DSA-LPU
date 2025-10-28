84865a3cafce1d6312b02949f58139fc85401fa9
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-10-28 23:47:49 +0530

    Create 29_deletionInLL.cpp

---
+++ 29_deletionInLL.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+
+    
+};
+
+void print(Node* head){
+    Node* p = head;
+
+    while(p!=NULL){
+        cout << p -> data << " ";
+        p = p -> next;
+    }
+}
+
+void deleteAtPos(Node* head, int pos){
+    Node* p = head;
+    Node* q = nullptr;
+
+    for(int i=1; i<pos; i++){
+        q = p;
+        p = p -> next;
+    }
+    q -> next = p -> next;
+    delete p;
+    
+}
+int main() {
+    Node* head = new Node(12);
+    head -> next = new Node(22);
+    head -> next -> next = new Node(32);
+
+    int pos = 2;
+    int val = 100;
+    deleteAtPos(head, pos);
+    print(head);
+    
+    return 0;
+}
