eeb5ff44dd70f826f28beae6a0190f394f431e77
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-11-03 22:42:00 +0530

    Create 37_deletion_at_any_index_in_doubly_LL.cpp

---
+++ 37_deletion_at_any_index_in_doubly_LL.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+        prev = NULL;
+    }
+    
+};
+
+void print(Node* head){
+    Node* p = head;
+
+    while(p!=NULL){
+        cout << p -> data << " ";
+        p = p -> next;
+    }
+}
+
+void deleteAtPosInDoublyLL(Node* head, int pos){
+    Node* p = head;
+    Node* q = nullptr;
+
+    for(int i=1; i<pos; i++){
+        q = p;
+        p = p -> next;
+    }
+    q -> next = p -> next;
+    p -> next -> prev = q;
+    delete p;
+    
+}
+int main() {
+    Node* head = new Node(12);
+    head -> next = new Node(22);
+    head -> next -> next = new Node(32);
+
+    int pos = 2;
+    // int val = 100;
+    deleteAtPosInDoublyLL(head, pos);
+    print(head);
+    
+    return 0;
+}
