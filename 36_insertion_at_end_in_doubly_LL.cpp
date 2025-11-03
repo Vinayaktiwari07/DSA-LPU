7b4e991604415780e629e1f7bd6ad6fc029ac058
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-11-03 22:41:56 +0530

    Create 36_insertion_at_end_in_doubly_LL.cpp

---
+++ 36_insertion_at_end_in_doubly_LL.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int value){
+        data = value;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+
+void insertionAtEndInDoublyLL(Node* &head, int val){
+    Node* p = head;
+    // Node* q = nullptr;
+    
+    // for(int i=1; i<pos; i++){
+    //     q = p;
+    //     p = p -> next;
+    // }
+    while(p -> next != NULL){
+        p = p -> next;
+    }
+    
+    Node* newNode = new Node(val);
+    // newNode -> next = p;
+    p -> next = newNode;
+    newNode -> prev = p;
+    // q -> next = newNode;
+    // p -> prev = newNode;
+    // newNode -> prev = q;
+    // head = newNode;
+}
+void Print(Node* head){
+    Node* p = head;
+
+    while(p != NULL){
+        cout << p -> data << " ";
+        p = p -> next;
+    }
+}
+int main() {
+    Node* head = new Node(12);
+    head -> next = new Node(22);
+    head -> next -> next = new Node(32);
+    head -> next -> next -> next = new Node(42);
+
+    // int pos = 3;
+    int val = 100;
+    insertionAtEndInDoublyLL(head, val);
+    Print(head);
+    return 0;
+}
