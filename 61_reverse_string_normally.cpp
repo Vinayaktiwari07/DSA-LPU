01f4baefe3db5d713cb784b9e488445adc0450cb
Vinayak Tiwari <vinayaktiwari00326@gmail.com>
2025-11-18 17:35:23 +0530

    Create 61_reverse_string_normally.cpp

---
+++ 61_reverse_string_normally.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s = "Hello";
+    int n = 5;
+
+    for(int i=n-1; i>=0; i--){
+        cout << s[i];
+    }
+    return 0;
+}
